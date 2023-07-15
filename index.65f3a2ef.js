26a37709aab5c19bc9d9a58435dea716d6eb777d
NataDent <125708241+NataDent@users.noreply.github.com>
2023-07-15 17:49:37 +0000

    Deploying to gh-pages from @ NataDent/goit-js-hw-10@a8819388cdfa7b8d7b4fdd2f5e86e05eaaf9b10f 🚀

---
+++ index.65f3a2ef.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector(".breed-select");fetch("".concat("https://api.thecatapi.com/v1/images/").concat(":image_id","/breeds?api_key=").concat("live_EoiEHQFHGA9fSObTg9LvrdmbjMOo1jxuHEpYvjlDJSD2ztHdQxpughVUKKy6jSwQ")).then((function(t){if(!t.ok)throw new Error(t.statusText);return t.json()})).catch((function(t){return console.log(t)}))}();
+//# sourceMappingURL=index.65f3a2ef.js.map
