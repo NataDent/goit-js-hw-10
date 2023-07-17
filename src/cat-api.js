a2138735d8a1a108d2e677d6e7f86004cdcddb03
Наталья ЧеркашинаКоцько <cherkashyna.n@gmail.com>
2023-07-17 16:28:10 +0300

    5

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,26 +1,33 @@
 const ROOT_URL = 'https://api.thecatapi.com/v1';
 const X_API_KEY = 'live_FRHznCkDEBA2Kr8ISlGtYpZ1nMdTqK6BKbcxoCO4g6OBxRHHn0KzH6YXTjSxLe1V';
+const API_HEADERS = new Headers({
+    'Content-Type': 'application/json',
+    'x-api-key' : X_API_KEY,
+});
 
-export function fetchBreeds() {
-    fetch(`${ROOT_URL}/breeds?api_key=${X_API_KEY}`)
+function fetchBreeds(){   
+       return fetch(`${ROOT_URL}breeds`,{method:"GET", headers:API_HEADERS})
         .then(resp => {
-            console.log(resp);
             if (!resp.ok) {
-                throw new Error(resp.status);
+                throw new Error(resp.statusText);
             }
-            return resp.json();
+           return resp.json()})
+        .then(res => {
+           return res.map(obj => ({
+                id: obj.id ,
+                name: obj.name 
+            }))
         })
-        .catch(error => console.log(error))
+        .catch(error =>console.log(error)); 
 }
+    
+function fetchCatByBreed(breedId){     
+        return fetch(`${ROOT_URL}images/search?breed_ids=${breedId}`, {method:"GET",headers:API_HEADERS}).then(req => 
+            {if(!req.ok){
+                throw new Error(req.error);
+            }
+            return req.json();
+        })
+    }
 
-export function fetchCatByBreed(breedId) {
-    return fetch(`${ROOT_URL}/images/search?api_key=${X_API_KEY}breed_id=${breedId}`)
-        .then(resp => {
-            console.log(resp);
-            if (!resp.ok) {
-                throw new Error(resp.status);
-            }
-            return resp.json();
-        })
-        .catch(error => console.log(error))
-}
+export { fetchBreeds, fetchCatByBreed };
