5b251916b25a3460b3a3f942776bfe27f7eaa987
Наталья ЧеркашинаКоцько <cherkashyna.n@gmail.com>
2023-07-17 22:52:21 +0300

    14

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,52 +1,23 @@
 import axios from "axios";
 
-const ROOT_URL = 'https://api.thecatapi.com/v1/';
-const X_API_KEY = 'live_FRHznCkDEBA2Kr8ISlGtYpZ1nMdTqK6BKbcxoCO4g6OBxRHHn0KzH6YXTjSxLe1V';
-const API_HEADERS = new Headers({
-    'Content-Type': 'application/json',
-    'x-api-key' : X_API_KEY,
-});
-
 axios.defaults.headers.common["x-api-key"] = "live_FRHznCkDEBA2Kr8ISlGtYpZ1nMdTqK6BKbcxoCO4g6OBxRHHn0KzH6YXTjSxLe1V";
 axios.defaults.baseURL = 'https://api.thecatapi.com/v1/';
-const instance = axios.create({
-    baseURL: `${ROOT_URL}`, 
-    
-    headers: API_HEADERS
-});
-axios.get(`${ROOT_URL}breeds`)
-  .then((response) => {
-    console.log(response.data);
-    console.log(response.status);
-    console.log(response.statusText);
-    console.log(response.headers);
-    console.log(response.config);
-  })
-    .catch(error => console.log(error));
 
-// function fetchBreeds() { 
-//     return resp.then(resp => {
-//             if (!resp.ok) {
-//                 throw new Error(resp.statusText);
-//             }
-//            return resp.json()})
-//         .then(res => {
-//            return res.map(obj => ({
-//                 id: obj.id ,
-//                 name: obj.name 
-//             }))
-//         })
-//         .catch(error =>console.log(error)); 
-// }
-    
-function fetchCatByBreed(breedId) {   
+
+function fetchBreeds() {
+    return axios('breeds').then(({ data }) => {
+        return data;
+    });
+}
+
+// function fetchCatByBreed(breedId) {   
    
-        return fetch(`${ROOT_URL}images/search?breed_ids=${breedId}`, {method:"GET",headers:API_HEADERS}).then(req => 
-            {if(!req.ok){
-                throw new Error(req.error);
-            }
-            return req.json();
-        })
-    }
+//         return fetch(`${ROOT_URL}images/search?breed_ids=${breedId}`, {method:"GET",headers:API_HEADERS}).then(req => 
+//             {if(!req.ok){
+//                 throw new Error(req.error);
+//             }
+//             return req.json();
+//         })
+//     }
 
-export { fetchBreeds, fetchCatByBreed };
+export { fetchBreeds };
--- src/index.js
+++ src/index.js
@@ -1,78 +1,19 @@
-import axios from "axios";
 import { fetchBreeds, fetchCatByBreed }  from "./cat-api";
 import SlimSelect from 'slim-select'
+import 'slim-select / dist / slimselect.css';
 
 const select = document.querySelector('.breed-select');
-const selectBox = document.querySelector('.cat-info')
-console.log(select)
-// const selectBox = document.querySelector('.cat-info')
+const selectBox = document.querySelector('.cat-info');
 
+fetchBreeds()
+    .then((data) => {
+        createBreedsList(data);
+    })
+    .catch(error => { console.log(error) });
 
-const ROOT_URL = 'https://api.thecatapi.com/v1/ ';
-const X_API_KEY = 'live_FRHznCkDEBA2Kr8ISlGtYpZ1nMdTqK6BKbcxoCO4g6OBxRHHn0KzH6YXTjSxLe1V';
-
-const API_HEADERS = new Headers({
-    'Content-Type': 'application/json',
-    'x-api-key' : X_API_KEY,
-});
-
-function fetchBreeds(){
-       return fetch(`${ROOT_URL}breeds`,{method:"GET", headers:API_HEADERS})
-           .then(resp => {
-            console.log(resp)
-            if (!resp.ok) {
-                throw new Error(resp.statusText);
-            }
-           return resp.json()})
-        .then(res => {
-           return res.map(obj => ({
-                id: obj.id ,
-                name: obj.name
-            }))
-        })
-        .catch(error =>console.log(error));
-}
-    
-
-
-fetchBreeds().then(createBreedsList);
 function createBreedsList(data) {
-  const result = data.map(({ id, name }) => ({
-    text: name,
-    value: id
-  }));
-
-  const emptyObj = { text: " ", value: " " };
-    result.unshift(emptyObj);
-    
-     new SlimSelect({
-    select: '.breed-select',
-    data: result,
-  });
+    const result = data.map(({ id, name }) => {
+        `<option value ='${id}'>${name}</option>`
+    }).join('');
+    select.innerHTML = result;
 }
-// const ROOT_URL = 'https://api.thecatapi.com/v1/ ';
-// const X_API_KEY = 'live_FRHznCkDEBA2Kr8ISlGtYpZ1nMdTqK6BKbcxoCO4g6OBxRHHn0KzH6YXTjSxLe1V';
-
-// const API_HEADERS = new Headers({
-//     'Content-Type': 'application/json',
-//     'x-api-key' : X_API_KEY,
-// });
-
-function fetchBreeds(){
-       return fetch(`${ROOT_URL}breeds`,{method:"GET", headers:API_HEADERS})
-           .then(resp => {
-            console.log(resp)
-            if (!resp.ok) {
-                throw new Error(resp.statusText);
-            }
-           return resp.json()})
-        .then(res => {
-           return res.map(obj => ({
-                id: obj.id ,
-                name: obj.name
-            }))
-        })
-        .catch(error =>console.log(error));
-}
-    
-fetchBreeds().then(data => console.log(data))
