eecedee25eac2965f41531ca5afc4473eb1fd87b
Наталья ЧеркашинаКоцько <cherkashyna.n@gmail.com>
2023-07-17 19:09:19 +0300

    11

--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
-// import axios from "axios";
+import axios from "axios";
 import { fetchBreeds, fetchCatByBreed }  from "./cat-api";
 import SlimSelect from 'slim-select'
 
 const select = document.querySelector('.breed-select');
 const selectBox = document.querySelector('.cat-info')
 
-// fetchBreeds().then(createBreedsList);
-// function createBreedsList(data) {
-//   const result = data.map(({ id, name }) => ({
-//     text: name,
-//     value: id
-//   }));
+fetchBreeds().then(createBreedsList);
+function createBreedsList(data) {
+  const result = data.map(({ id, name }) => ({
+    text: name,
+    value: id
+  }));
 
-//   const emptyObj = { text: " ", value: " " };
-//     result.unshift(emptyObj);
+  const emptyObj = { text: " ", value: " " };
+    result.unshift(emptyObj);
     
-//      new SlimSelect({
-//     select: '.breed-select',
-//     data: result,
-//   });
-// }
-const ROOT_URL = 'https://api.thecatapi.com/v1/ ';
-const X_API_KEY = 'live_FRHznCkDEBA2Kr8ISlGtYpZ1nMdTqK6BKbcxoCO4g6OBxRHHn0KzH6YXTjSxLe1V';
+     new SlimSelect({
+    select: '.breed-select',
+    data: result,
+  });
+}
+// const ROOT_URL = 'https://api.thecatapi.com/v1/ ';
+// const X_API_KEY = 'live_FRHznCkDEBA2Kr8ISlGtYpZ1nMdTqK6BKbcxoCO4g6OBxRHHn0KzH6YXTjSxLe1V';
 
-const API_HEADERS = new Headers({
-    'Content-Type': 'application/json',
-    'x-api-key' : X_API_KEY,
-});
+// const API_HEADERS = new Headers({
+//     'Content-Type': 'application/json',
+//     'x-api-key' : X_API_KEY,
+// });
 
 function fetchBreeds(){   
        return fetch(`${ROOT_URL}breeds`,{method:"GET", headers:API_HEADERS})
