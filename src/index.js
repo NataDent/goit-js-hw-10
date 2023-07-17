fd224f12ad8511a04136aa822b0c7a678deeb7b9
Наталья ЧеркашинаКоцько <cherkashyna.n@gmail.com>
2023-07-17 15:41:30 +0300

    4

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
 import axios from "axios";
 import fetchBreeds  from "./cat-api";
 import fetchCatByBreed from "./cat-api";
+import SlimSelect from 'slim-select'
 
 axios.defaults.headers.common["x-api-key"] = "live_FRHznCkDEBA2Kr8ISlGtYpZ1nMdTqK6BKbcxoCO4g6OBxRHHn0KzH6YXTjSxLe1V";
 axios.defaults.baseURL = 'https://api.thecatapi.com/v1';
 
-const ROOT_URL = 'https://api.thecatapi.com/v1';
+
+// const ROOT_URL = 'https://api.thecatapi.com/v1';
 const select = document.querySelector('.breed-select');
 const selectBox = document.querySelector('.cat-info')
 
-select.addEventListener('select', onSelect)
+fetchBreeds().then(createBreedsList);
+function createBreedsList(data) {  
+  const result = data.map(({ id, name }) => ({
+    text: name,
+    value: id
+  }));
 
-
-
-// function onSelect(e) {
-//     fetchCatByBreed(breedId);
-
-// }
+  const emptyObj = { text: " ", value: " " };
+    result.unshift(emptyObj);
+    
+     new SlimSelect({
+    select: '.breed-select',
+    data: result,    
+  });    
+}
