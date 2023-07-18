f455bc9dcba802a2d9dbdae0460da744a52cf8b0
Наталья ЧеркашинаКоцько <cherkashyna.n@gmail.com>
2023-07-19 01:27:16 +0300

    24

--- src/index.js
+++ src/index.js
@@ -3,25 +3,27 @@ import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import "notiflix/dist/notiflix-3.2.6.min.css";
+import './css/styles.css';
 
 const select = document.querySelector('.breed-select');
 const box = document.querySelector('.cat-info');
 const loader = document.querySelector('.loader');
 const errorEl = document.querySelector('.error');
 
-loader.classList.add('visible');
+loader.setAttribute('hidden', true)
 errorEl.setAttribute('hidden', true);
 
 fetchBreeds()
     .then((data) => {
-        
+        loader.removeAttribute('hidden');  
         createBreedsList(data);
     })
     .catch((err) => {
         Notify.warning(err.message)
     })
     .finally(() => {
-       loader.classList.remove('visible');
+        loader.setAttribute('hidden', true);
+        box.removeAttribute('hidden');
     })
 
 function createBreedsList(data) {  
@@ -42,12 +44,14 @@ function createBreedsList(data) {
 select.addEventListener('change', onChange);
 
 function onChange(e) {
+    
     const id = e.target.value;
-    loader.classList.add('visible');
-    box.classList.remove('visible');
+    loader.removeAttribute('hidden');
+    box.setAttribute('hidden', true)
     
     fetchCatByBreed(id)
         .then(({ data }) => {
+            box.setAttribute('hidden', true)
             const imgUrl = data[0].url;
             const catName = data[0].breeds[0].name;
             const catTemperament = data[0].breeds[0].temperament;
@@ -60,15 +64,15 @@ function onChange(e) {
             Notify.warning(err.message)
         })
         .finally(() => {
-           loader.classList.remove('visible');
-           box.classList.add('visible');
+          loader.setAttribute('hidden', true)
+          box.removeAttribute('hidden');
         })
 }
 
 function createMarkupCatInfo(img, name, temperament, description) {
 
     return `<img src="${img}"width="400" height=auto alt="Some cat ${name}">
-<h2>${name}</h2>
+<h2 class="name">${name}</h2>
 <ul class="list">
   <li>
     <p><b class='text-accent'>Temperament:&nbsp;</b>${temperament}</p>
